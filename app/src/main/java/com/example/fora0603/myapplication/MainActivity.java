9c181a0604d5f6b96bd9e9ded3490364047cb915
himnada <himnada@gmail.com>
2016-07-04 12:03:57 +0900

    asd

--- app/src/main/java/com/example/fora0603/myapplication/MainActivity.java
+++ app/src/main/java/com/example/fora0603/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         finish();
+//        https://github.com/himnada/android-test.git
     }
 }
